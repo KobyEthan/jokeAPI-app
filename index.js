58780d3a69562cc739fa347c35223e3d11b8be66
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-24 18:51:47 -0500

    selected the public API and modified the get method to hit up the all jobs endpoint.

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", async (req,res) => {
-        res.render("index.ejs");
+  try{
+     const response = await axios.get("curl -X GET /path/to/api/v1/jobs")
+     const result = response.data;
+     console.log(result);
+     res.render("index.ejs", { data: result});
+  } catch (error){
+     console.log("Failed to make request: ", error.message);
+     res.render("index.ejs", {
+        error: error.message,
+     });
+  }
+});
+
+app.post("/", async (req, res) => {
+try {
+   console.log(req.body);
+
+} catch (error) {
+        
+}
+
 });
 
 app.listen(port, () => {
