3c1a060339d3146bb4d92cfab29db9e202962c9e
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-25 21:38:15 -0500

    Changed the API that will be used for the project. Also made some progress the get and post methods, as well as the structure of the ejs file.

--- index.js
+++ index.js
@@ -5,31 +5,35 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req,res) => {
-  try{
-     const response = await axios.get("curl -X GET /path/to/api/v1/jobs")
-     const result = response.data;
-     console.log(result);
-     res.render("index.ejs", { data: result});
-  } catch (error){
-     console.log("Failed to make request: ", error.message);
-     res.render("index.ejs", {
-        error: error.message,
-     });
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Any");
+    const result = response.data;
+    console.log(result);
+    res.render("index.ejs", { joke: result });
+  } catch (error) {
+    console.log("Failed to make request: ", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
   }
 });
 
-app.post("/", async (req, res) => {
-try {
-   console.log(req.body);
-
-} catch (error) {
-        
-}
-
+app.post("/get-joke", async (req, res) => {
+  try {
+    const { category } = req.body;
+    const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}`);
+    const result = response.data;
+    console.log(result);
+    res.send(result);
+  } catch (error) {
+    console.log("Failed to get joke: ", error.message);
+    res.status(500).send("Failed to get joke");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
+  console.log(`Server running on port: ${port}`);
+});
