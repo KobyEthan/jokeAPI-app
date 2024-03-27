06c087b480c8ccce5226e92a864562d4e1f4b291
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-26 21:18:26 -0500

    Worked on the custom joke funtionality and made two selector elements in the ejs file for the user to select a custom joke.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
+    const response = await axios.get("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit");
     const result = response.data;
     console.log(result);
     res.render("index.ejs", { joke: result });
@@ -21,29 +21,37 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/get-any-joke", async (req, res) => {
+app.post("/get-custom-joke", async (req, res) => {
+   try {
+    console.log(req.body.flags);
+    console.log(req.body.category);
+     const category = req.body.category;
+     const flags = req.body.flags;
+     if (category && flags){
+     const response = await axios.get(`https://v2.jokeapi.dev/joke/${category}?blacklistFlags=${flags}`);
+     const result = response.data;
+     console.log(result);
+     res.render("index.ejs", { joke: result });
+    }else {
+      res.render("index.ejs") //do something with this later
+    }
+   } catch (error) {
+     console.log("Failed to get joke: ", error.message);
+     res.status(500).send("Failed to get joke");
+   }
+ });
+
+ app.post("/get-any-joke", async (req, res) => {
   try {
     const response = await axios.get(`https://v2.jokeapi.dev/joke/Any`);
     const result = response.data;
     console.log(result);
     res.render("index.ejs", { joke: result });
   } catch (error) {
-    console.log("Failed to get joke: ", error.message);
-    res.status(500).send("Failed to get any joke");
+    console.log("Failed to get any joke: ", error.message);
+    res.status(500).send("Failed to get joke");
   }
 });
-
-app.post("/get-good-joke", async (req, res) => {
-   try {
-     const response = await axios.get(`https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`);
-     const result = response.data;
-     console.log(result);
-     res.render("index.ejs", { joke: result });
-   } catch (error) {
-     console.log("Failed to get good joke: ", error.message);
-     res.status(500).send("Failed to get joke");
-   }
- });
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
