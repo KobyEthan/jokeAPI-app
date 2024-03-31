c565cdb9bbf475012bb11dda3322ebca5db823fe
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-03-31 00:06:16 -0500

    Fixed deployment issue with Heroku.

--- index.js
+++ index.js
@@ -61,6 +61,4 @@ app.post("/get-custom-joke", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+app.listen(process.env.PORT || port);
